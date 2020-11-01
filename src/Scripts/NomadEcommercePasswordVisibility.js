83487c1406bf6f979f41048262236974e017e391
Robert Guida <40727103+robguida@users.noreply.github.com>
2020-11-01 12:54:47 -0500

    Add files via upload
    Initial Commit

---
+++ src/Scripts/NomadEcommercePasswordVisibility.js
@@ -0,0 +1,25 @@
+﻿$.fn.NomadPasswordVisibility = function () {
+    let elemPassword = this.find('input[type="password"]').first();
+    let elemCheckBox = this.find('input[type="checkbox"]').first();
+    let elemLabel = this.find('span').first();
+
+    function togglePasswordVisibility() {
+       if (elemPassword.attr('type') === 'password') {
+            elemPassword.attr('type', 'text');
+            elemLabel.html('Hide Password');
+            elemCheckBox.prop('checked', true);
+        } else {
+            elemPassword.attr('type', 'password');
+            elemLabel.html('Show Password');
+            elemCheckBox.prop('checked', false);
+        }
+    }
+    elemLabel
+        .css('cursor', 'pointer')
+        .on('click', function () {
+            togglePasswordVisibility();
+        });
+    elemCheckBox.on('click', function () {
+        togglePasswordVisibility();
+    });
+}
